517c79641baaedfda22c8e37d0b5616dacce1611
Aga Skorska <skorskaga@gmail.com>
2021-02-21 22:30:26 +0000

    Add flatpickr (error for range-slider)

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,8 @@
 import {select, templates} from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 
 class Booking {
@@ -17,6 +19,7 @@ class Booking {
     const thisBooking = this;
     thisBooking.dom = {};
     thisBooking.dom.wrapper = thisBooking.element;
+
     const generatedHTML = templates.bookingWidget();
     const toDOM = utils.createDOMFromHTML(generatedHTML);
     const wrapper = thisBooking.dom.wrapper;
@@ -24,13 +27,17 @@ class Booking {
 
     thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
     thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
-    console.log(thisBooking.dom.peopleAmount);
+    thisBooking.dom.date = document.querySelector(select.widgets.datePicker.wrapper);
+    console.log(thisBooking.dom.date);
+    thisBooking.dom.hour = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets() {
     const thisBooking = this;
     new AmountWidget(thisBooking.dom.hoursAmount);
     new AmountWidget(thisBooking.dom.peopleAmount);
+    new DatePicker(thisBooking.dom.date);
+    new HourPicker(thisBooking.dom.hour);
   }
 }
 
